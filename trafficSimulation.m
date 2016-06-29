19d503730831b418c3060daff10449c767b93f64
Jacob <jacob_ruth@brown.edu>
2016-06-29 11:12:22 -0400

    Finished Commenting

--- trafficSimulation.m
+++ trafficSimulation.m
@@ -13,8 +13,8 @@ function trafficSimulation()
     %% initialize car positions and velocities
     cars_1 = zeros(2*numCars, 1);
     cars_2 = zeros(2*numCars, 1);
-    v0_base1 = 0.89;
-    v0_base2 = 0.889;
+    v0_base1 = 0.91;
+    v0_base2 = 0.9;
     
     for i = 1:numCars
         cars_1(i) = (i-1) * len/numCars + mu*sin(2*pi*i/numCars);
@@ -33,7 +33,7 @@ function trafficSimulation()
 %      toc;
     
 %     save('refStats.mat','ref_1','ref_2');
-      load('refStats.mat','ref_1','ref_2');
+      load('refStats919.mat','ref_1','ref_2');
       std(getHeadways(ref_1(1:numCars)))
       std(getHeadways(ref_2(1:numCars)))
     
@@ -82,8 +82,8 @@ function trafficSimulation()
             f = F(ref_2, u(2),u(1));
             fprintf('f is %f \n', f)
             Df = jacobian(ref_2, u(1), u(2), w);
-            neww = w(1)*(u(1)-newGuess(1)) + w(2)*(u(2) - newGuess(2)) - stepSize
-            u = u - Df^(-1)*[f;w(1)*(u(1)-newGuess(1)) + w(2)*(u(2) - newGuess(2))]
+            neww = w(1)*(u(1)-newGuess(1)) + w(2)*(u(2) - newGuess(2)) - stepSize;
+            u = u - Df^(-1)*[f;neww];
             k = k + 1;
         end
         
@@ -124,6 +124,16 @@ function trafficSimulation()
 %     hold off
     
     %% lift, evolve, restrict
+    % sigma - the current value of the std, used to seed the lifting
+    % ref   - a reference state, used to seed the lifting
+    % t     - the duration to evaluate the lifted parameters
+    % p     -  parameter which will degrade the accuracy of the lifting
+    %               operator.  Should be kept at p = 1 by default.
+    % v0    - the optimal velocity parameter for this state
+    % RETURNS: 
+    % sigma     - the std. of the headways after restricting has occured
+    % new_state - the final state of the evolution, which can be used as a
+    %               future reference state
     function [sigma,new_state] = ler(sigma,ref,t,p,v0)
         lifted = lift(sigma, p, getHeadways(ref(1:numCars)),v0);
          [~,evo] = ode45(@microsystem,[0 t],[lifted;v0],options);
@@ -133,13 +143,26 @@ function trafficSimulation()
              new_state = evo(end,1:2*numCars)';
          end
     end
-    
+    %% Finite Difference Quotient
+    %  ref - reference state to base the lifting 
+    %  sigma - the current value of the std. at which point to approximate
+    %       the time derivative
+    %  v0 - the velocity parameter for this state
+    %  RETURNS:
+    %  dif - the difference which approximates the time derivative
     function dif = F(ref, sigma,v0)
         r0 = ler(sigma, ref, tskip, 1,v0);
         r1 = ler(sigma, ref, tskip+delta, 1,v0);
         dif = (r1-r0)/delta;
     end
-    
+    %% Jacobian for newton's method
+    % ref - The previous reference state used to compute F.
+    % sigma - the current value of sigma 
+    % v0 - the velocity parameter for this state
+    % w - the secant direction
+    % J- The Jacobian, which will be given by
+    % | F_sigma    F_v0  |
+    % | w_sigma    w_vo  |
     function J = jacobian(ref, sigma, v0,w)
         J = zeros(2);
         unchanged = F(ref, sigma, v0);
@@ -147,8 +170,19 @@ function trafficSimulation()
         J(1,2) = (F(ref, sigma,v0 + delv0) - unchanged)/delv0;
         J(2,:) = w';
     end
-
+    
     %% lifting operator
+    % s 	-   the target std. for this state
+    % p     -   parameter which will degrade the accuracy of the lifting
+    %           operator.  Should be kept at p = 1 by default.
+    % hways -   headways of the cars at a reference state
+    % v0    -   the goal velocity at this state
+    % RETURNS: 
+    % new   -   the lifted vector of cars and their positions. The cars
+    %           will be reset to new positions with the first car centered 
+    %            at 0, and with headways that will give the desired std. of
+    %            s.  The cars' new velocities will be given directly by the
+    %            optimal velocity function.
     function new = lift(s, p, hways, v0)
         del = p * s/std(hways) * (hways - mean(hways)) + mean(hways);
         new = zeros(length(hways)*2,1);
@@ -172,6 +206,7 @@ function trafficSimulation()
     function v = optimalVelocity(headway,v0)
         v = v0 * (tanh(headway - h) + tanh(h));
     end
+
     %% function to calculate headways of car vector
     %  v   - column vector of the cars' positions
     %  returns:
