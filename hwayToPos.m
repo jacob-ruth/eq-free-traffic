6b1fb3957fea8a056dd107720b510da419b1ed72
Jacob <jacob_ruth@brown.edu>
2016-08-04 14:04:22 -0400

    Added missing constant

--- hwayToPos.m
+++ hwayToPos.m
@@ -1,4 +1,5 @@
     function positions = hwayToPos(headways)
+    numCars = 60;
         positions = zeros(size(headways));
         for ih = 2:numCars
             positions(ih) = positions(ih - 1) + headways(ih);
