2fd6a1a39b292697fdf40aa9ef1cb3c42cf58ce7
jacob-ruth <jacob_ruth@brown.edu>
2016-08-10 11:23:56 -0400

    Restriction check for m~

---
+++ restrictionCheck.m
@@ -0,0 +1,19 @@
+load('bigDataMap.mat', 'evecs', 'evals', 'eps', 'allData');
+numRestrict = 1;
+restrictDiff = zeros(numRestrict, 2);
+restrictDiffS = zeros(numRestrict, 2);
+%nystrom error comparison, calculated the following way:
+%   'correct' coordinates are given by the evec coord
+%   approximated coordinates are given by diff map restrict, when the value
+%   is removed from the data set
+%   better approximated coordinates are given by diff map restrict, when
+%   the value is still included in the data set
+for iRestrict = 1:size(evecs, 1)
+    evecs2 = [evecs(1:iRestrict - 1, :) ; evecs(iRestrict + 1 : end, :)]; 
+    allData2 = [allData(:, 1:iRestrict - 1) , allData(:,iRestrict + 1 : end)]; 
+    restricted = diffMapRestrict(allData(:,iRestrict),evals,evecs2,allData2,eps);
+    restrictedS = diffMapRestrict(allData(:,iRestrict),evals,evecs,allData,eps);
+    origRestricted = evecs(iRestrict, :);
+    restrictDiff(iRestrict,:) = origRestricted - restricted';
+    restrictDiffS(iRestrict,:) = origRestricted - restrictedS';
+end
