2faff617554ca9e0dc58135f0f2170c0922cf01e
tchin <tracy_chin@brown.edu>
2016-08-11 16:46:27 -0400

    Update diffMapRestrict.m
    Additional comments

--- diffMapRestrict.m
+++ diffMapRestrict.m
@@ -8,10 +8,11 @@
 % returns diffusion map embedding for new data using the Nystrom extension
 % in a column vector
 function pnew = diffMapRestrict(newData,evals,evecs,origData,eps)
+% pdist2 much faster than for loop. Finds pairwise distances between newData and each entry in origData.
 dist = pdist2(newData',origData')';
-w = basicKernel(dist);
-k = (1/sum(w))*w;
-pnew = (evecs' * k)./diag(evals);
+w = basicKernel(dist);      % apply Gaussian kernel to weight smaller distances more heavily
+k = (1/sum(w))*w;           % normalize weights to add to 1
+pnew = (evecs' * k)./diag(evals);   % new embedding given by weighted sum of eigencoordinates already in the data
 
     function af = basicKernel(s)
         af = exp(-s.^2/eps^2);
