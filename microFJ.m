7407943d616c22f14154e33d34b2aff7b8be1522
rsantorella <santorr2@tcnj.edu>
2016-08-11 16:34:15 -0400

    Commented microFJ

--- microFJ.m
+++ microFJ.m
@@ -1,9 +1,14 @@
-function F = microFJ(sys1,sys2,M,N,L, v0, tau)
-%% parameters and variables
+%% microFJ gives the function to zero for finding equilibrium
+% sys1 - reference state
+% sys2 - reference state for phase shift condition
 % N - discretization points
 % M - number of cars (M must divide N)
 % L - length of road
 % tau - momentum constant;
+% Returns:
+% F - the function to zero for an equilibrium solution
+function F = microFJ(sys1,sys2,M,N,L, v0, tau)
+
 k = N/M;
 mu = sys1(end);
 c = sys1(end - 1);     % wavespeed
@@ -31,21 +36,6 @@ F = [ LN*u + optimalVelocity(shift*u, v0) - optimalVelocity(u, v0) + mu.*ones(N,
     ln_consv*u - L; ...
     (LD*u)'*(u-u0)...
     ];
-%% Jacobian computation
-%{
-if nargout > 1
-    J0 = diag(optimalVHway(shift*u,v0));
-    J1 = diag(optimalVHway(u,v0));
-    Jc = (-2*c*tau*D2+D)*u;
-    Jmu = ones(N,1);
-
-    J  = sparse(...
-        [LN+J0-J1, Jc, Jmu;...
-        ln_consv, 0, 0;...
-        (LD*u)'+ u'*LD - u0'*LD, 0, 0]...
-        );
-end
-%}
 
 %% optimal veloctiy function
     function v = optimalVelocity(headway,v0)
