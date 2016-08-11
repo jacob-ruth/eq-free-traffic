f796f640637028b0aa32d2e4e93447dd4a28429d
tchin <tracy_chin@brown.edu>
2016-08-11 16:49:05 -0400

    Update diffusionMap.m
    Additional comments.

--- diffusionMap.m
+++ diffusionMap.m
@@ -14,8 +14,10 @@ function [vec,val] = diffusionMap(epsilon,distMatrix,k)
     
     %return first k eigenvalues/vectors
     val = seval(2:end,2:end);
-    vec = sevec(:,2:end);    
-    % create a Markov matrix
+    vec = sevec(:,2:end);
+    
+    %% create a Markov matrix
+    % af - affinity matrix to normalize
     function M = markovify(af)
         M = zeros(size(af));
         rsum = sum(af,2);
@@ -24,16 +26,21 @@ function [vec,val] = diffusionMap(epsilon,distMatrix,k)
         end
     end
 
+    %% create symmetric Markov matrix to optimize eigenvector calculation
+    % af - affinity matrix to normalize
     function SM = smarkovify(af)
         d = diag(sum(af,2).^-.5);
         SM = d*af*d;
     end
 
-    % kernel function
+    %% Gaussian kernel function
+    % s - vector of distances
     function af = basicKernel(s)
         af = exp(-s.^2/epsilon^2);
     end
 
+    %% alternative kernel function
+    % s - vector of distances
     function fancy = pKernel(s)
         af = basicKernel(s);
         sqrtp = sqrt(sum(af,2)); % column vector of sums across rows
