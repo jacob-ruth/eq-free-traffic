e152adf193ed404070d0e75030768b2c666e9496
Jacob <jacob_ruth@brown.edu>
2016-08-11 16:19:01 -0400

    fixed comments and removed old code

--- trafficDiffMapBifurcation2D.m
+++ trafficDiffMapBifurcation2D.m
@@ -5,30 +5,13 @@ numCars = 60;           % number of cars
 tskip = 300;            % times for evolving
 delta = 500;
 stepSize = .0002;        % step size for the secant line approximation
-delSigma = 0.00001;     % delta sigma used for finite difference of F
-delv0 = 0.00001;        % delta v0 used for finite difference of F
 tolerance = 10^(-12);    % tolerance for Newton's method
 options = odeset('AbsTol',10^-8,'RelTol',10^-8); % ODE 45 options
 foptions = optimset('TolFun',tolerance);              % fsolve options
 
 %% load diffusion map data
-%{
-load('bigData.mat', 'trafficOutput2');
- allData = getHeadways(trafficOutput2(1:numCars, :), len);
-numEigvecs = 2;                                         % number of eigenvectors to return
-[evecs, evals, eps] = runDiffMap(allData,numEigvecs);   % run the diffusion map
-%}
 load('bigDataMap.mat', 'evecs', 'evals', 'eps', 'allData');
 
-% calculate which eigenvectors are most significant
-%{
-r = zeros(numEigvecs, 1);
-r(1) = 1;
-for j = 2:numEigvecs
-    r(j) = linearFit(evecs,j);
-end
-%}
-%{
 [~, max1] = max(allData,[],1);  % locate the max headway for each data point
 % plot eigenvector 1 vs eigenvector 2, colored by max headway location
 figure;
@@ -50,7 +33,6 @@ colormap(jet);
 xlabel('\Phi_1', 'FontSize',15);
 ylabel('\Phi_2', 'FontSize',15);
 title('\Phi_1 vs. \Phi_2 Colored by Standard Deviation of the Headways','FontSize',15);
-%}
 
 %% initialize secant continuation
 steps = 100;                                % number of steps to take around the curve
@@ -83,7 +65,7 @@ for iEq=1:steps
     newGuess = [embed_2; v0_base2] + stepSize *(w/norm(w)); % first guess on the secant line
                                 	
     %% alternate Newton's method using fsolve
-    u = fsolve(@(u)FW(u,allData,w,newGuess,evecs,evals,eps), newGuess,foptions)
+    u = fsolve(@(u)FW(u,allData,w,newGuess,evecs,evals,eps), newGuess,foptions);
     
     bif(:,iEq) = u;                                            % save the new solution
     scatter(u(1), u(2), 'k.'); drawnow;
@@ -110,7 +92,7 @@ xlabel('v_0');
 ylabel('\sigma');
 
 %% function to zero for fsolve
-% u         - the current value of (sigma, v0) that we're trying to find
+% u         - the current value of (phi_1, v0) that we're trying to find
 %               with Newton's method
 % ref       - the most recent reference state
 % W         - the slope of the secant line for arc length continuation
@@ -119,24 +101,27 @@ ylabel('\sigma');
 %
 % RETURNS:
 % fw    - the functions F and w evaluated at these parameters
-    function fw = FW(u,ref,W,newGuess,evecs,evals,lereps)
+    function fw = FW(u,allData,W,newGuess,evecs,evals,lereps)
         fw = zeros(3,1);
-        fw(1:2) = F(ref,u(1:2),u(end),evecs,evals,lereps);
+        fw(1:2) = F(allData,u(1:2),u(end),evecs,evals,lereps);
         fw(end) = W'*(u - newGuess);
         fprintf('F = %d \n\n',norm(fw));
     end
 
 %% lift, evolve, restrict
-% sigma - the current value of the std, used to seed the lifting
-% ref   - a reference state, used to seed the lifting
-% t     - the duration to evaluate the lifted parameters
-% p     -  parameter which will degrade the accuracy of the lifting
-%               operator.  Should be kept at p = 1 by default.
-% v0    - the optimal velocity parameter for this state
+% newval     - the current value of the macrovariables,
+%                 used to seed the lifting
+% orig       - the data set, for use in lifting
+% t          - the duration to evaluate the lifted parameters
+% v0         - the optimal velocity parameter for this state
+% eigvecs    - eigenvectors from diffusion map
+% eigvals    - eigenvalues from diffusion map
+% lereps     - epsilon used in diffusionmap
+% tReference - a second state to lift to, if desired
 % RETURNS:
-% sigma     - the std. of the headways after restricting has occured
-% new_state - the final state of the evolution, which can be used as a
-%               future reference state
+% sigma      - the macro-state of the headways after evolving for t
+% sigma2     - the macro-state of the headways, after evolving for t +
+%               tReference
     function [sigma, sigma2] = ler(newval,orig,t,v0,eigvecs,eigvals,lereps,tReference)
         fprintf('Lifting to: %f \n', newval(1));
         fprintf('\t and %f \n', newval(2));
@@ -154,14 +139,18 @@ ylabel('\sigma');
     end
 
 %% Finite Difference Quotient
-%  ref - reference state to base the lifting
-%  sigma - the current value of the std. at which point to approximate
-%       the time derivative
-%  v0 - the velocity parameter for this state
+% allData    - data set for use in lifting
+% sigma      - the current value of the macrovariable at which point to approximate
+%               the time derivative
+% v0         - the velocity parameter for this state
+% eigvecs    - eigenvectors from diffusion map
+% eigvals    - eigenvalues from diffusion map
+% lereps     - epsilon used in diffusionmap
+
 %  RETURNS:
 %  dif - the difference which approximates the time derivative
-    function dif = F(ref, sigma,v0,eigvecs,eigvals,lereps)
-        [r0, r1] = ler(sigma, ref, tskip, v0, eigvecs,eigvals,lereps, delta);
+    function dif = F(allData, sigma,v0,eigvecs,eigvals,lereps)
+        [r0, r1] = ler(sigma, allData, tskip, v0, eigvecs,eigvals,lereps, delta);
         dif = (r1-r0)/delta;
     end
 
