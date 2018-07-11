c201a649eeab56ecda9746c1884c259e8cf44545
rsantorella <santorr2@tcnj.edu>
2018-07-11 09:05:00 -0400

    Added header

--- hwayToPos.m
+++ hwayToPos.m
@@ -1,9 +1,10 @@
-    function positions = hwayToPos(headways)
-    numCars = 60;
-        positions = zeros(size(headways));
-        for ih = 2:numCars
-            positions(ih) = positions(ih - 1) + headways(ih);
-        end
-        positions = circshift(positions, 1);
-    end
+% takes headways of cars and returns their positions
+function positions = hwayToPos(headways)
+numCars = 30;
+positions = zeros(size(headways));
+for ih = 2:numCars
+    positions(ih) = positions(ih - 1) + headways(ih);
+end
+positions = circshift(positions, 1); % shift back
+end
 
