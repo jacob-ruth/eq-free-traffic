b98042a265bf6336ce62fbd9c10f0b0e2859a8a1
rsantorella <santorr2@tcnj.edu>
2018-10-02 16:24:29 -0400

    Updated for N=30

--- microsystemBifurcation.m
+++ microsystemBifurcation.m
@@ -1,19 +1,19 @@
 %% function microsystemBifurcation creates the bifurcation diagram using the microsystem only
 function microsystemBifurcation()
-h = 1.2;
+h = 2.4;
 len = 60;
-numCars = 60;
-mu = .1;
+numCars = 30;
 
+v0_base1 = 1.14;
+v0_base2 = 1.135;
+
+%{
+options = odeset('AbsTol',10^-8,'RelTol',10^-8); % ODE 45 options
 %% initialize car positions and velocities
 cars_1 = zeros(2*numCars, 1);
 cars_2 = zeros(2*numCars, 1);
-
-origv01 = 0.91;
-origv02 = 0.9;
-v0_base1 = origv01;
-v0_base2 = origv02;
-
+mu = .1;
+finalTime  = 50000;
 for i = 1:numCars
     cars_1(i) = (i-1) * len/numCars + mu*sin(2*pi*i/numCars);
     cars_1(i+numCars) = optimalVelocity(len/numCars, v0_base1);
@@ -23,45 +23,49 @@ for i = 1:numCars
 end
 
 %% save new reference state (if you want)
-%{
+
 tic;
-[t1,allTime_1] = ode45(@microsystem,[0 finalTime],cars_1, options, v0_base1);
+[~,allTime_1] = ode45(@(t,y)microsystem(t,y, [v0_base1 len h]),[0 finalTime],cars_1, options);
 ref_1 = allTime_1(end,:)';
-[t2,allTime_2] = ode45(@microsystem,[0 finalTime],cars_2, options, v0_base2);
+[~,allTime_2] = ode45(@(t,y)microsystem(t,y, [v0_base2 len h]),[0 finalTime],cars_2, options);
 ref_2 = allTime_2(end,:)';
 toc;
 save('refStats.mat','ref_1','ref_2');
 %}
 
 %% or you can load them instead
-load('refStats919.mat','ref_1','ref_2');
+load('refStats.mat','ref_1','ref_2');
+
 hways1 = getHeadways(ref_1(1:numCars));
 hways2 = getHeadways(ref_2(1:numCars));
 
 %% Bifurcation on micro
-steps = 110;
+steps = 200;
 stepSize = 0.025;
-
 cguess = -0.8581;
 
 foptions = optimset( 'TolFun',1e-15,'TolX',1e-15, 'Display', 'off');
 
 [~, max1] = max(hways1);
 [~, max2] = max(hways2);
-hways1 = circshift(hways1, [-max1  + 30, 0]);    % shift so max headway is in the middle-ish
-hways2 =circshift (hways2, [-max2 + 30, 0]);
+hways1 = circshift(hways1, [-max1  + 10, 0]);    % shift so max headway is in the middle-ish
+hways2 =circshift (hways2, [-max2 + 10, 0]);
 discreteScaling = 1;
 
 % add more discretization points for smoother curve
-hways1 = interp1(1:60,hways1,...
+hways1 = interp1(1:numCars,hways1,...
     linspace(1,numCars,numCars * discreteScaling))';
-hways2 = interp1(1:60,hways2,...
+hways2 = interp1(1:numCars,hways2,...
     linspace(1,numCars,numCars * discreteScaling))';
 
 sys1 = [hways1; cguess; 0; v0_base1];
 sys2 = [hways2; cguess; 0; v0_base2];
 
 bif  = zeros(numCars + 3, steps);
+
+figure; hold on;
+scatter(v0_base1, std(hways1), 100,'r*');
+scatter(v0_base2, std(hways2), 100, 'r*');
 
 for iMic = 1:steps
     w = sys2 - sys1;        % tangent vector
@@ -76,15 +80,14 @@ for iMic = 1:steps
     % set variables for next iteration
     sys1 = sys2;
     sys2 = u;
+    
+    scatter(bif(end,iMic), std(r), 400, 'b.'); drawnow;
 end
 
 %save('microBif.mat', 'bif');
 %% plot results
 figure;
 scatter(bif(end,:), std(bif(1:numCars,:)),50,'b.');
-
-figure;
-scatter(1:steps,bif(end,:));
 
 %% function to minimize with fsolve
 % var       - state to vary in order to minimize fw
